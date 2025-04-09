d10062379ebd1b744ba0d287e57892e97f7bad85
Werner Goebl <goebl@mdw.ac.at>
2025-04-09 11:40:52 +0200

    Prettify js code

--- demo.js
+++ demo.js
@@ -1,97 +1,121 @@
 const demoFiles = {
-  "Beethoven Op53_1": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op53_HenleUrtext/master/Beethoven_Op53_1-HenleUrtext.mei",
-    "performances": {
-      "53 P02 Performance A": "./eval/Op53_1_P02-A.boe.mid.maps.expansion-default.json",
-      "53 P02 Performance B": "./eval/Op53_1_P02-B.boe.mid.maps.expansion-default.json",
-      "53 P02 Performance C": "./eval/Op53_1_P02-C.boe.mid.maps.expansion-default.json",
-      "53 P04 Performance A": "./eval/Op53_1_P04-A.boe.mid.maps.expansion-default.json",
-      "53 P04 Performance B": "./eval/Op53_1_P04-B.boe.mid.maps.expansion-default.json",
-      "53 P04 Performance C": "./eval/Op53_1_P04-C.boe.mid.maps.expansion-default.json",
-      "53 P07 Performance A": "./eval/Op53_1_P07-A.boe.mid.maps.expansion-default.json",
-      "53 P07 Performance B": "./eval/Op53_1_P07-B.boe.mid.maps.expansion-default.json",
-      "53 P08 Performance A": "./eval/Op53_1_P08-A.boe.mid.maps.expansion-default.json",
-      "53 P08 Performance B": "./eval/Op53_1_P08-B.boe.mid.maps.expansion-default.json",
-      "53 P08 Performance C": "./eval/Op53_1_P08-C.boe.mid.maps.expansion-default.json"
-    }
+  'Beethoven Op53_1': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op53_HenleUrtext/master/Beethoven_Op53_1-HenleUrtext.mei',
+    performances: {
+      '53 P02 Performance A': './eval/Op53_1_P02-A.boe.mid.maps.expansion-default.json',
+      '53 P02 Performance B': './eval/Op53_1_P02-B.boe.mid.maps.expansion-default.json',
+      '53 P02 Performance C': './eval/Op53_1_P02-C.boe.mid.maps.expansion-default.json',
+      '53 P04 Performance A': './eval/Op53_1_P04-A.boe.mid.maps.expansion-default.json',
+      '53 P04 Performance B': './eval/Op53_1_P04-B.boe.mid.maps.expansion-default.json',
+      '53 P04 Performance C': './eval/Op53_1_P04-C.boe.mid.maps.expansion-default.json',
+      '53 P07 Performance A': './eval/Op53_1_P07-A.boe.mid.maps.expansion-default.json',
+      '53 P07 Performance B': './eval/Op53_1_P07-B.boe.mid.maps.expansion-default.json',
+      '53 P08 Performance A': './eval/Op53_1_P08-A.boe.mid.maps.expansion-default.json',
+      '53 P08 Performance B': './eval/Op53_1_P08-B.boe.mid.maps.expansion-default.json',
+      '53 P08 Performance C': './eval/Op53_1_P08-C.boe.mid.maps.expansion-default.json',
+    },
   },
-  "Beethoven Op53_2": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op53_HenleUrtext/master/Beethoven_Op53_2-HenleUrtext.mei",
-    "performances": {
-      "P02 Performance A": "./eval/Op53_2_P02-A.boe.mid.maps.json",
-      "P02 Performance B": "./eval/Op53_2_P02-B.boe.mid.maps.json",
-      "P02 Performance C": "./eval/Op53_2_P02-C.boe.mid.maps.json",
-      "P04 Performance A": "./eval/Op53_2_P04-A.boe.mid.maps.json",
-      "P04 Performance B": "./eval/Op53_2_P04-B.boe.mid.maps.json",
-      "P04 Performance C": "./eval/Op53_2_P04-C.boe.mid.maps.json",
-      "P07 Performance A": "./eval/Op53_2_P07-A.boe.mid.maps.json",
-      "P07 Performance B": "./eval/Op53_2_P07-B.boe.mid.maps.json",
-      "P07 Performance C": "./eval/Op53_2_P07-C.boe.mid.maps.json",
-      "P08 Performance A": "./eval/Op53_2_P08-A.boe.mid.maps.json",
-      "P08 Performance B": "./eval/Op53_2_P08-B.boe.mid.maps.json",
-      "P08 Performance C": "./eval/Op53_2_P08-C.boe.mid.maps.json"
-    }
+  'Beethoven Op53_2': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op53_HenleUrtext/master/Beethoven_Op53_2-HenleUrtext.mei',
+    performances: {
+      'P02 Performance A': './eval/Op53_2_P02-A.boe.mid.maps.json',
+      'P02 Performance B': './eval/Op53_2_P02-B.boe.mid.maps.json',
+      'P02 Performance C': './eval/Op53_2_P02-C.boe.mid.maps.json',
+      'P04 Performance A': './eval/Op53_2_P04-A.boe.mid.maps.json',
+      'P04 Performance B': './eval/Op53_2_P04-B.boe.mid.maps.json',
+      'P04 Performance C': './eval/Op53_2_P04-C.boe.mid.maps.json',
+      'P07 Performance A': './eval/Op53_2_P07-A.boe.mid.maps.json',
+      'P07 Performance B': './eval/Op53_2_P07-B.boe.mid.maps.json',
+      'P07 Performance C': './eval/Op53_2_P07-C.boe.mid.maps.json',
+      'P08 Performance A': './eval/Op53_2_P08-A.boe.mid.maps.json',
+      'P08 Performance B': './eval/Op53_2_P08-B.boe.mid.maps.json',
+      'P08 Performance C': './eval/Op53_2_P08-C.boe.mid.maps.json',
+    },
   },
-  "Beethoven Op31_No2_2": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No2_HenleUrtext/master/Beethoven_Op31_No2_2-HenleUrtext.mei",
-    "performances": {
-      "Op31-No2_2 D1": "./eval/REC041_12-19_1133.boe.mid.maps.expansion-default.json",
-      "Op31-No2_2 D2": "./eval/REC042_12-19_1153.boe.mid.maps.expansion-default.json",
-      "Op31-No2_2 D3": "./eval/REC043_12-19_1202.boe.mid.maps.expansion-default.json",
-      "Op31-No2_2 D4": "./eval/REC044_12-19_1210.boe.mid.maps.expansion-default.json"
-    }
+  'Beethoven Op31_No2_2': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No2_HenleUrtext/master/Beethoven_Op31_No2_2-HenleUrtext.mei',
+    performances: {
+      'Op31-No2_2 D1': './eval/REC041_12-19_1133.boe.mid.maps.expansion-default.json',
+      'Op31-No2_2 D2': './eval/REC042_12-19_1153.boe.mid.maps.expansion-default.json',
+      'Op31-No2_2 D3': './eval/REC043_12-19_1202.boe.mid.maps.expansion-default.json',
+      'Op31-No2_2 D4': './eval/REC044_12-19_1210.boe.mid.maps.expansion-default.json',
+    },
   },
-  "Beethoven Op31_No3_1": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No3_HenleUrtext/master/Beethoven_Op31_No3_1-HenleUrtext.mei",
-    "performances": {
-      "Op31-No3_1 P01 Performance A": "./eval/Op31_No3_1_P01-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P01 Performance B": "./eval/Op31_No3_1_P01-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P01 Performance C": "./eval/Op31_No3_1_P01-C.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P05 Performance A": "./eval/Op31_No3_1_P05-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P05 Performance B": "./eval/Op31_No3_1_P05-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P05 Performance C": "./eval/Op31_No3_1_P05-C.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P06 Performance A": "./eval/Op31_No3_1_P06-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P06 Performance B": "./eval/Op31_No3_1_P06-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_1 P06 Performance C": "./eval/Op31_No3_1_P06-C.boe.mid.maps.expansion-default.json"
-    }
+  'Beethoven Op31_No3_1': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No3_HenleUrtext/master/Beethoven_Op31_No3_1-HenleUrtext.mei',
+    performances: {
+      'Op31-No3_1 P01 Performance A': './eval/Op31_No3_1_P01-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P01 Performance B': './eval/Op31_No3_1_P01-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P01 Performance C': './eval/Op31_No3_1_P01-C.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P05 Performance A': './eval/Op31_No3_1_P05-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P05 Performance B': './eval/Op31_No3_1_P05-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P05 Performance C': './eval/Op31_No3_1_P05-C.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P06 Performance A': './eval/Op31_No3_1_P06-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P06 Performance B': './eval/Op31_No3_1_P06-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_1 P06 Performance C': './eval/Op31_No3_1_P06-C.boe.mid.maps.expansion-default.json',
+    },
   },
-  "Beethoven Op31_No3_2": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No3_HenleUrtext/master/Beethoven_Op31_No3_2-HenleUrtext.mei",
-    "performances": {
-      "Op31-No3_2 P01 Performance A": "./eval/Op31_No3_2_P01-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P01 Performance B": "./eval/Op31_No3_2_P01-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P01 Performance C": "./eval/Op31_No3_2_P01-C.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P05 Performance A": "./eval/Op31_No3_2_P05-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P05 Performance B": "./eval/Op31_No3_2_P05-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P05 Performance C": "./eval/Op31_No3_2_P05-C.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P05 Performance D": "./eval/Op31_No3_2_P05-D.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P06 Performance A": "./eval/Op31_No3_2_P06-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P06 Performance B": "./eval/Op31_No3_2_P06-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_2 P06 Performance C": "./eval/Op31_No3_2_P06-C.boe.mid.maps.expansion-default.json"
-    }
+  'Beethoven Op31_No3_2': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No3_HenleUrtext/master/Beethoven_Op31_No3_2-HenleUrtext.mei',
+    performances: {
+      'Op31-No3_2 P01 Performance A': './eval/Op31_No3_2_P01-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P01 Performance B': './eval/Op31_No3_2_P01-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P01 Performance C': './eval/Op31_No3_2_P01-C.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P05 Performance A': './eval/Op31_No3_2_P05-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P05 Performance B': './eval/Op31_No3_2_P05-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P05 Performance C': './eval/Op31_No3_2_P05-C.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P05 Performance D': './eval/Op31_No3_2_P05-D.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P06 Performance A': './eval/Op31_No3_2_P06-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P06 Performance B': './eval/Op31_No3_2_P06-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_2 P06 Performance C': './eval/Op31_No3_2_P06-C.boe.mid.maps.expansion-default.json',
+    },
   },
-  "Beethoven Op31_No3_3": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No3_HenleUrtext/master/Beethoven_Op31_No3_3-HenleUrtext.mei",
-    "performances": {
-      "Op31-No3_3 P01 Performance A": "./eval/Op31_No3_3_P01-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P01 Performance B": "./eval/Op31_No3_3_P01-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P01 Performance C": "./eval/Op31_No3_3_P01-C.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P05 Performance A": "./eval/Op31_No3_3_P05-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P05 Performance B": "./eval/Op31_No3_3_P05-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P05 Performance C": "./eval/Op31_No3_3_P05-C.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P05 Performance D": "./eval/Op31_No3_3_P05-D.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P06 Performance A": "./eval/Op31_No3_3_P06-A.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P06 Performance B": "./eval/Op31_No3_3_P06-B.boe.mid.maps.expansion-default.json",
-      "Op31-No3_3 P06 Performance C": "./eval/Op31_No3_3_P06-C.boe.mid.maps.expansion-default.json"
-    }
+  'Beethoven Op31_No3_3': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op31_No3_HenleUrtext/master/Beethoven_Op31_No3_3-HenleUrtext.mei',
+    performances: {
+      'Op31-No3_3 P01 Performance A': './eval/Op31_No3_3_P01-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P01 Performance B': './eval/Op31_No3_3_P01-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P01 Performance C': './eval/Op31_No3_3_P01-C.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P05 Performance A': './eval/Op31_No3_3_P05-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P05 Performance B': './eval/Op31_No3_3_P05-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P05 Performance C': './eval/Op31_No3_3_P05-C.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P05 Performance D': './eval/Op31_No3_3_P05-D.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P06 Performance A': './eval/Op31_No3_3_P06-A.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P06 Performance B': './eval/Op31_No3_3_P06-B.boe.mid.maps.expansion-default.json',
+      'Op31-No3_3 P06 Performance C': './eval/Op31_No3_3_P06-C.boe.mid.maps.expansion-default.json',
+    },
   },
-  "Beethoven Op119_No1_1": {
-    "meiFile": "https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op119_BreitkopfHaertel/master/Beethoven_Op119_Nr01-Breitkopf.mei",
-    "performances": {
-      "119 Pianist 1": "./eval/Beethoven_Op119_1_maps.json",
-      "119 Pianist 2": "./eval/Beethoven_Op119_1_maps.json"
-    }
-  }
+  'Beethoven Op119_No1_1': {
+    meiFile:
+      'https://raw.githubusercontent.com/trompamusic-encodings/Beethoven_Op119_BreitkopfHaertel/master/Beethoven_Op119_Nr01-Breitkopf.mei',
+    performances: {
+      '119 Pianist 1': './eval/Beethoven_Op119_1_maps.json',
+      '119 Pianist 2': './eval/Beethoven_Op119_1_maps.json',
+    },
+  },
+  'Schumann: Die Kartenlegerin': {
+    meiFile:
+      'https://raw.githubusercontent.com/TogethernessProject/Schumann_Kartenlegerin/main/Schumann_Op31_No2_Die-Kartenlegerin_Es-Dur_Peters.mei',
+    performances: {
+      'Duo 1 Round 1': './eval/duo1-PostR-1Schumann.maps.json',
+      'Duo 1 Round 2': './eval/duo1-PostR-2Schumann.maps.json',
+      'Duo 1 Round 3': './eval/duo1-PostR-3Schumann.maps.json',
+    },
+  },
+  'Fauré: Automne': {
+    meiFile:
+      'https://raw.githubusercontent.com/TogethernessProject/Faure_Automne/main/Faure_Op18_No3_Automne_h-Moll_Hamelle.mei',
+    performances: {
+      'Duo 1 Round 2': './eval/duo1-PostR-2Faure.maps.json',
+      'Duo 1 Round 3': './eval/duo1-PostR-3Faure.maps.json',
+    },
+  },
 };
 
 const defaultPiece = '';
